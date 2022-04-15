89ccfcbb4b4bc787f30e36eb8c5a479197709821
prkl8r <puerkel@gmail.com>
2022-04-15 14:04:18 -0500

    fixed model and admin files

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,5 +1,4 @@
-from django.test import TestCase, \
-    Client
+from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
@@ -22,9 +21,18 @@ class AdminSiteTests(TestCase):
 
     def test_users_listed(self):
         # Test users are listed on the user page
-        url = reverse("admin:core_user_changeList")
+        url = reverse("admin:core_user_changelist")
         res = self.client.get(url)
 
         # Looks for a 200 and that the output can find the expected contents
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        # Test that the user edit page works
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        # /admin/core/user/x
+
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
