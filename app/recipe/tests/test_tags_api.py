5aa8be74a18c1fc7f7e38a88c8d642bb4c6811c1
prkl8r <puerkel@gmail.com>
2022-04-18 16:01:08 -0500

    Added Create Tag feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.test import TestCase
 
 from rest_framework import status
-from rest_framework.test import APIClient, force_authenticate
+from rest_framework.test import APIClient
 
 from core.models import Tag
 
@@ -33,10 +33,9 @@ class PrivateTagsApiTests(TestCase):
         self.user = get_user_model().objects.create_user(
             'test@example.com',
             'password12345',
-            'Test Name'
         )
         self.client = APIClient()
-        self.client = force_authenticate(self.user)
+        self.client.force_authenticate(self.user)
 
     def test_retrieve_tags(self):
         # Test retreive tags
@@ -56,7 +55,6 @@ class PrivateTagsApiTests(TestCase):
         user2 = get_user_model().objects.create_user(
             "other@example.com",
             "password1234",
-            "Test2 NAme"
         )
         Tag.objects.create(user=user2, name='Chicken')
         tag = Tag.objects.create(user=self.user, name="Comfort Food")
@@ -67,4 +65,21 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]["name"], tag.name)
 
+    def test_create_tag_successful(self):
+        # Test creating a new tag
+        payload = {"name": "Test Tag"}
+        self.client.post(TAGS_URL, payload)
 
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload["name"]
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid_name(self):
+        # Test creating a new tag with inv
+        payload = {"name": ""}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -108,6 +108,7 @@ class PublicUserApiTests(TestCase):
     def test_create_token_missing_field(self):
         # Test email an pword are required
         res = self.client.post(TOKEN_URL, {"email": "one", "password": ""})
+        self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_retrieve_user_unauthorized(self):
