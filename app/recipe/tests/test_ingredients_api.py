80ab890cfde5b91119c43dfaee7c5df8e86783ef
prkl8r <puerkel@gmail.com>
2022-04-20 13:14:57 -0500

    Feature add ingredients

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -70,3 +70,29 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]["name"], ingredient.name)
+
+    def test_create_ingredient_success(self):
+        # Test that the user is able to create a new ingredient.
+        # Arrange
+        payload = {"name": "Bacon"}
+        self.client.post(INGREDIENTS_URL, payload)
+
+        # ACT
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload["name"]
+        ).exists()
+
+        # Assert
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid_name(self):
+        # Test creating a new ingredient with an invalid name
+        # Arrange
+        payload = {"name": ""}
+
+        # Act
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        # ASSERT
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
